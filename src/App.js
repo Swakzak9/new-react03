b03dc70ccb6d9bb5b80dd1eb999aa0f25b1509d6
Aliu Zayd <swagzak.jr@gmail.com>
2024-08-19 12:30:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './index.css'
+class App extends Component{
+  constructor(props){
+    super(props) 
+      this.state = {
+        person: {
+          fullname: 'Kelly Thurmani',
+          bio: 'A psychologist from Ibadan',
+          imgSrc: '/Images/p-r.jpeg' ,
+          profession:  'Psychological Doctor' 
+        },
+        shows: false,
+        mountTime:0,
+      }
+    
+  }
+  componentDidMount(){
+    this.interval= setInterval(()=>{
+    this.setState((prevState) =>({
+      mountTime:prevState.mountTime + 1
+    }));
+  }, 1000);
+}
 
-function App() {
+componentWillUnmount(){
+  clearInterval(this.interval);
+}
+toggleShow= () => {
+  this.setState((prevState)=> ({
+  shows:!prevState.shows
+}));
+
+};
+
+render() {
+  const { person, shows, mountedTime } = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={this.toggleShow}>
+        {shows ? 'Hide Profile' : 'Show Profile'}
+      </button>
+      {shows && (
+        <div>
+          <h1>{person.fullName}</h1>
+          <p>{person.bio}</p>
+          <img src={person.imgSrc} alt="Profile" />
+          <p>{person.profession}</p>
+        </div>
+      )}
+      <p>Time since component mounted: {mountedTime} seconds</p>
     </div>
   );
+}
 }
 
 export default App;
